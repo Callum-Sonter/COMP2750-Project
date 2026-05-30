35ad714b090684f3939e4633f2f552d44a50970f
jordanpuleitu <jordan.puleitu@students.mq.edu.au>
2026-05-30 15:33:39 +1000

    firebase-config.js
    Firebase config to use to integrate htmls, js, css, etc

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCNvKJZAsiX227CmM8jECSXeOJnBMY-_WQ",
+  authDomain: "assessment-3-e4563.firebaseapp.com",
+  projectId: "assessment-3-e4563",
+  storageBucket: "assessment-3-e4563.firebasestorage.app",
+  messagingSenderId: "673526224139",
+  appId: "1:673526224139:web:3fc3856c5c18e4aeaf3e0e",
+  measurementId: "G-YJC4Z0QEW0"
+};
